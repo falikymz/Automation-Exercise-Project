04701eb26dd9c24307acce27ba610ab0f5370ce3
falikymz <kymz7331@gmail.com>
2023-11-26 16:05:59 +0300

    test base updated

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -3,6 +3,7 @@ package utilities;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.github.javafaker.Faker;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.*;
@@ -153,6 +154,72 @@ public abstract class TestBase {
         }
     }
 
+    //Create Account
+    public void createAccount(String email ,String pwd){
+        Faker faker =new Faker();
+
+        //go to url
+        driver.get("https://www.automationexercise.com/");
+
+        //click signup/login button
+        driver.findElement(By.xpath("//a[text()=' Signup / Login']")).click();
+
+        // Name Value
+        driver.findElement(By.xpath("//input[@name='name']")).sendKeys(faker.name().fullName());
+
+        //Email Value
+        driver.findElement(By.xpath("(//input[@name='email'])[2]")).sendKeys(email);
+
+        //signup button
+        driver.findElement(By.xpath("//button[@data-qa='signup-button']")).click();
+
+        //Gender Radio Button
+        WebElement genderButton = driver.findElement(By.xpath("//*[@id='id_gender1']"));
+        genderButton.click();
+
+        //*Password Box
+        WebElement pwdBox=driver.findElement(By.cssSelector("#password"));
+        pwdBox.sendKeys(pwd);
+
+        // Locate to Dropdown Menu
+        WebElement day =driver.findElement(By.xpath("//select[@id='days']"));
+        WebElement month =driver.findElement(By.xpath("//select[@id='months']"));
+        WebElement year =driver.findElement(By.xpath("//select[@id='years']"));
+
+        day.sendKeys("11",Keys.ENTER);
+        month.sendKeys("April",Keys.ENTER);
+        year.sendKeys("2004",Keys.ENTER);
+
+        //*Full Name Box
+        WebElement firstName=driver.findElement(By.id("first_name"));
+        firstName.sendKeys(faker.name().firstName(),Keys.TAB,faker.name().lastName());
+
+        //*Adress Box
+        WebElement adressBox =driver.findElement(By.id("address1"));
+        String address =faker.address().fullAddress();
+        adressBox.sendKeys(address);
+
+        //*Country Box
+        WebElement countryBox= driver.findElement(By.id("country"));
+        countryBox.sendKeys("Canada");
+
+        //*State Box
+        WebElement stateBox = driver.findElement(By.xpath("//input[@id='state']"));
+        stateBox.sendKeys(faker.address().state());
+
+        //*City
+        driver.findElement(By.xpath("//input[@id='city']")).sendKeys(faker.address().cityName());
+
+        //*Zipcode
+        driver.findElement(By.xpath("//input[@id='zipcode']")).sendKeys(faker.address().zipCode());
+
+        //*Mobile Number
+        driver.findElement(By.xpath("//input[@id='mobile_number']")).sendKeys(faker.phoneNumber().cellPhone());
+
+        //Create Button
+        driver.findElement(By.xpath("//*[@data-qa='create-account']")).submit();
+
+    }
 
 
 
