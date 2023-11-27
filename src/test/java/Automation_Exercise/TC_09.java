16388ef5b82da83d9ccc09908f69c2f5e5fc6190
MuammerUcar <muammerucar15092021@gmail.com>
2023-11-27 04:02:45 +0300

    27-11-2023

--- src/test/java/Automation_Exercise/TC_09.java
+++ src/test/java/Automation_Exercise/TC_09.java
@@ -1,6 +1,68 @@
 package Automation_Exercise;
 
-public class TC_09 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class TC_09 extends TestBase {
 
 
+    @Test
+    public void TestCase09() {
+        rapor("Chrome","Automation Exercise Test");
+        extentTest=extentReports.createTest("Automation Exercise","Test Case 09");
+
+        //1. Launch browser(1. Tarayıcıyı başlatın)
+        extentTest.info("Tarayıcı başlatıldı");
+
+        //2. Navigate to url 'http://automationexercise.com'(2. 'http://automationexercise.com' URL'sine gidin)
+        driver.get("http://automationexercise.com");
+        extentTest.info("'http://automationexercise.com' sitesine gidildi");
+
+        //3.Verify that home page is visible successfully(3. Ana sayfanın başarıyla göründüğünü doğrulayın)
+        WebElement logoElementi = driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
+        Assert.assertTrue(logoElementi.isDisplayed());
+        extentTest.info("Ana sayfanın başarılı bir şekilde göründüğü doğrulandı");
+
+
+        //4.Click on 'Products' button(4. 'Ürünler' düğmesine tıklayın)
+        driver.findElement(By.xpath("(//a[@href='/products'])")).click();
+        extentTest.info("Ürünler düğmesine tıklandı");
+
+        // 5. Verify user is navigated to ALL PRODUCTS page successfully(5. Kullanıcının TÜM ÜRÜNLER sayfasına başarıyla yönlendirildiğini doğrulayın)
+        String  actualAllProductsButton=driver.findElement(By.xpath("(//h2[@class='title text-center'])")).getText();
+        String expectedAllProductsButton="ALL PRODUCTS";
+        Assert.assertEquals(expectedAllProductsButton,actualAllProductsButton);
+        extentTest.info("Kullanıcının 'TÜM ÜRÜNLER' sayfasına başarıyla yönlendirildiği doğrulandı");
+
+
+        // 6. Enter product name in search input and click search button(6. Arama girişine ürün adını girin ve ara butonuna tıklayın)
+        driver.findElement(By.id("search_product")).sendKeys("Green Side Placket Detail T-Shirt");
+        driver.findElement(By.id("submit_search")).click();
+        extentTest.info("Arama girişine ürün adı girildi ve ara butonuna tıklandı");
+
+
+        Actions actions = new Actions(driver);
+        WebElement kids = driver.findElement(By.xpath("(//*[@href='/brand_products/Allen Solly Junior'])"));
+        actions.scrollToElement(kids).perform();
+
+        //7. Verify 'SEARCHED PRODUCTS' is visible(7. 'ARALANAN ÜRÜNLER'in görünür olduğunu doğrulayın)
+        WebElement seachedProducts = driver.findElement(By.xpath("(//*[@src='/get_product_picture/29'])"));
+        Assert.assertTrue(seachedProducts.isDisplayed());
+        extentTest.info("'ARALANAN ÜRÜNLER'in görünür olduğu doğrulandı");
+
+        //8. Verify all the products related to search are visible(8. Aramayla ilgili tüm ürünlerin görünür olduğunu doğrulayın)
+        String  actualAllSearchedProducts=driver.findElement(By.xpath("(//*[@class='title text-center'])")).getText();
+        String expectedAllSearchedProducts="SEARCHED PRODUCTS";
+        Assert.assertEquals(expectedAllSearchedProducts,actualAllSearchedProducts);
+        extentTest.info("Aramayla ilgili tüm ürünlerin görünür olduğu doğrulandı");
+        extentTest.pass("TEST BAŞARILI OLDU");
+
+        extentReports.flush();
+        driver.quit();
+    }
 }
---
+++ src/test/java/Automation_Exercise/TC_22.java
@@ -0,0 +1,57 @@
+package Automation_Exercise;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class TC_22 extends TestBase {
+
+    @Test
+    public void TestCase22() {
+
+        rapor("Chrome","Automation Exercise Test");
+        extentTest=extentReports.createTest("Automation Exercise","Test Case 09");
+
+        //1. Launch browser(1. Tarayıcıyı başlatın)
+        extentTest.info("Tarayıcı başlatıldı.");
+
+        //2. Navigate to url 'http://automationexercise.com'(2. 'http://automationexercise.com' URL'sine gidin)
+        driver.get("http://automationexercise.com");
+        extentTest.info("'http://automationexercise.com' sitesine gidildi.");
+
+        //3. Scroll to bottom of page(3. Sayfanın en altına gidin)
+        Actions actions = new Actions(driver);
+        //actions.sendKeys(Keys.END).perform();
+        extentTest.info("Sayfanın en altına gidildi.");
+
+        WebElement recommended = driver.findElement(By.xpath("(//*[@class='title text-center'])[2]"));
+        actions.scrollToElement(recommended).perform();
+
+        //4. Verify 'RECOMMENDED ITEMS' are visible(4. 'ÖNERİLEN ÖĞELER'in görünür olduğunu doğrulayın)
+
+        WebElement recommendedItems=driver.findElement(By.xpath("(//*[@class='title text-center'])[2]"));
+        Assert.assertTrue(recommendedItems.isDisplayed());
+        extentTest.info("Önerilen Ögelerin görünür olduğu doğrulandı.");
+        waitForSecond(5);
+
+        //5. Click on 'Add To Cart' on Recommended product(5. Önerilen üründe 'Sepete Ekle'ye tıklayın)
+        driver.findElement(By.xpath("(//*[@data-product-id='1'])[3]")).click();
+        extentTest.info("Önerilen üründe 'Sepete Ekle'ye tıklandı.");
+
+        //6. Click on 'View Cart' button(6. 'Sepeti Görüntüle' düğmesine tıklayın)
+        driver.findElement(By.xpath("(//*[@href='/view_cart'])[2]")).click();
+        extentTest.info("'Sepeti Görüntüle' düğmesine tıklandı.");
+
+        //7. Verify that product is displayed in cart page(7. Ürünün sepet sayfasında görüntülendiğini doğrulayın)
+        WebElement blueTop= driver.findElement(By.xpath("//a[@href='/product_details/1']"));
+        Assert.assertTrue(blueTop.isDisplayed());
+        extentTest.info("Ürünün sepet sayfasında görüntülendiği doğrulandı.");
+        extentTest.pass("TEST BAŞARILI OLDU");
+        extentReports.flush();
+        driver.quit();
+
+    }
+}
