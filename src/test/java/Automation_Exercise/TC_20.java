af0579c0deb70176fea7ecccc8365ce44e3fccf0
Ayşegül Temel <aysegultmll@gmail.com>
2023-12-02 01:14:22 +0300

    TC06 tamamlandı

---
+++ src/test/java/Automation_Exercise/TC_20.java
@@ -0,0 +1,63 @@
+package Automation_Exercise;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class TC_20 extends TestBase {
+    /*
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Click on 'Products' button
+4. Verify user is navigated to ALL PRODUCTS page successfully
+5. Enter product name in search input and click search button
+6. Verify 'SEARCHED PRODUCTS' is visible
+7. Verify all the products related to search are visible
+8. Add those products to cart
+9. Click 'Cart' button and verify that products are visible in cart
+10. Click 'Signup / Login' button and submit login details
+11. Again, go to Cart page
+12. Verify that those products are visible in cart after login as well
+     */
+
+
+    @Test
+    public void testCase20() {
+        rapor("Chrome","Automation Exercise Test");
+        extentTest=extentReports.createTest("Automation Exercise","Test Case 06");
+
+        //1. Launch browser
+        extentTest.info("Launch browser");
+
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        extentTest.info("Navigate to url 'http://automationexercise.com'");
+
+        //3. Click on 'Products' button
+        driver.findElement(By.xpath("//a[text()=' Products']")).click();
+        extentTest.info("Click 'Pruducts' button ");
+
+        //4. Verify user is navigated to ALL PRODUCTS page successfully
+        String actualHomePageButtonColor = driver.findElement(By.xpath("//a[text()=' Home']")).getAttribute("style");
+        String expectedHomePageButtonColor ="color: orange;";
+        Assert.assertEquals(expectedHomePageButtonColor,actualHomePageButtonColor);
+        extentTest.info("//3.Verify that home page is visible successfully");
+        extentTest.pass("Test Pass");
+
+        //5. Enter product name in search input and click search button
+       // 6. Verify 'SEARCHED PRODUCTS' is visible
+        //7. Verify all the products related to search are visible
+        //8. Add those products to cart
+        //9. Click 'Cart' button and verify that products are visible in cart
+       // 10. Click 'Signup / Login' button and submit login details
+       // 11. Again, go to Cart page
+        //12. Verify that those products are visible in cart after login as well
+
+        rapor("Chrome","Automation Exercise Test");
+        extentTest=extentReports.createTest("Automation Exercise","Test Case 06");
+
+
+
+    }
+}
